7c49c6f053628a97dca760a7f59708fc5cd51b91
JF <jasonhfields@gmail.com>
2015-07-09 07:33:38 -0500

    Nasty single quotes

--- imgur.py
+++ imgur.py
@@ -7,7 +7,7 @@ BASE_URL = "http://www.imgur.com/"
 
 def get_url_hash():
     counter = 0
-    url_hash = ''
+    url_hash = ""
     while counter < 5:
         random_letter = random.choice(string.letters)
         url_hash += random_letter
