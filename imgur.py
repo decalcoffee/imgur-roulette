354ac8b87a422215963f7bdb0757d49fddb8fbb2
JF <jasonhfields@gmail.com>
2015-07-09 09:11:48 -0500

    Gets n random urls for downloading

--- imgur.py
+++ imgur.py
@@ -16,12 +16,24 @@ def get_url_hash():
         counter += 1
     return url_hash
 
-def get_n_random_images(n):
+def get_n_random_image_urls(n):
+    urls = []
     for i in range(n):
         response = requests.get(BASE_URL + get_url_hash())
         while response.status_code != 200:
             response = requests.get(BASE_URL + get_url_hash())
-        print response
+        soup = BeautifulSoup(response.text, "html.parser")
+        # image_box is a reference to a div that houses the image on the page
+        image_box = soup.find("div", {"class" : "image textbox"})
+        image_element = image_box.find("img")
+        if image_element:
+            urls.append(image_element.attrs["src"])
+        else:
+            # it's a video/moving image
+            source_elements = image_box.findAll("source")
+            webm_element = source_elements[0]
+            urls.append(webm_element.attrs["src"])
+    return urls
 
 if __name__ == "__main__":
-    get_n_random_images(10)
+    print get_n_random_image_urls(10)
